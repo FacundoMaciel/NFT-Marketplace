0d1f54f20fcfd587409c069557b21bd1c3efc2ba
FacundoMaciel <facundomaciel12@hotmail.com>
2026-05-19 20:08:05 -0300

    Working on NFT marketplace

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -1,18 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+contract NFTMarketplace is Ownable {
 
-contract NFTMarketplace {
-
-    constructor() {
-        
+    struct Listing {
+        address seller;
+        address nftAddress;
+        uint256 tokenId;
+        uint256 price;
     }
+
+    mapping(address => mapping(uint256 => Listing)) listing;
+
+    constructor() Ownable(msg.sender) {}
     
-    // List NFT
+    function listNFT(address nftAddress_, uint256 tokenId_, uint256 price_) external {
+        Listing listing_ = Listing({
+            seller: msg.sender,
+            nftAddress: nftAddress_,
+            tokenId: tokenId_,
+            price: price_
+        })
+    }
 
-    // Buy NFT
-
-    // Cancel list
 
 }
