ece07e745d4b4a56eb0f52eb34b8e5c3310de863
FacundoMaciel <facundomaciel12@hotmail.com>
2026-05-19 19:32:54 -0300

    First Commit

---
+++ src/NFTMarketplace.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+
+
+contract NFTMarketplace {
+
+    constructor() {
+        
+    }
+    
+    // List NFT
+
+    // Buy NFT
+
+    // Cancel list
+
+}
