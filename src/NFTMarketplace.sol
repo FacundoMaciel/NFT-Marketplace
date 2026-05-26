ebb1ae0b8d98b7473b073ceb7fa6761ab1bfa16a
FacundoMaciel <facundomaciel12@hotmail.com>
2026-05-26 19:20:02 -0300

    cancelList function ready with validation and the event

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -5,7 +5,6 @@ import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
 contract NFTMarketplace is Ownable {
-
     struct Listing {
         address seller;
         address nftAddress;
@@ -15,29 +14,30 @@ contract NFTMarketplace is Ownable {
 
     mapping(address => mapping(uint256 => Listing)) listing;
 
+    event NTFTListed(address indexed seller, address indexed nftAddress, uint256 indexed tokenId, uint256 price);
+    event NTFTCanceled(address indexed seller, address indexed nftAddress, uint256 indexed tokenId);
+
     constructor() Ownable(msg.sender) {}
-    
+
     function listNFT(address nftAddress_, uint256 tokenId_, uint256 price_) external {
         require(price_ > 0, "Price cannot be 0");
         address owner_ = IERC721(nftAddress_).ownerOf(tokenId_);
         require(owner_ == msg.sender, "You are not the owner of the NFT");
 
-        Listing memory listing_ = Listing({
-            seller: msg.sender,
-            nftAddress: nftAddress_,
-            tokenId: tokenId_,
-            price: price_
-        });
+        Listing memory listing_ =
+            Listing({seller: msg.sender, nftAddress: nftAddress_, tokenId: tokenId_, price: price_});
 
         listing[nftAddress_][tokenId_] = listing_;
+
+        emit NTFTListed(msg.sender, nftAddress_, tokenId_, price_);
     }
 
     function cancelList(address nftAddress_, uint256 tokenId_) external {
         Listing memory listing_ = listing[nftAddress_][tokenId_];
-
         require(listing_.seller == msg.sender, "You are not the seller of the NFT");
 
+        delete listing[nftAddress_][tokenId_];
+
+        emit NTFTCanceled(msg.sender, nftAddress_, tokenId_);
     }
-
-
 }
